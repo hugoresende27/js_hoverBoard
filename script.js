b6cb93cc47afc6332be844197f6396e50f1f8dd6
hugoresende27 <Hugoresende27@gmail.com>
2021-11-19 17:17:50 +0000

    a

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const conteudo = document.getElementById('main')
-const cores = ['#e74c3c', '#8e44ad', '#3498db','#e67e22', '#2ecc71']
-const QUADRADOS = 500
+const cores = ['#e74c3c', '#8e44ad', '#3498db','#409C32','#e67e22', '#2ecc71','#265C1D','#BDD9BF','#412234'] //array de cores em hexadecimal
+const QUADRADOS = 500       //numero de quadrados
 
-for (let i=0; i < QUADRADOS ; i++)
+for (let i=0; i < QUADRADOS ; i++)      //loop para criar quadrados
 {
-    const quadrado = document.createElement('div')
-    quadrado.classList.add('quadrado')
+    const quadrado = document.createElement('div')//criar as divs 
+    quadrado.classList.add('quadrado')//adicionar as classes quadrado às divs
 
-    conteudo.appendChild(quadrado)
+    quadrado.addEventListener('mouseover', () => setCor(quadrado))      //função aplicada ao EventListener, mouseover vai chamar a função setCor e mouseout vai removerCor
+    quadrado.addEventListener('mouseout', () => removerCor(quadrado))
+
+    conteudo.appendChild(quadrado)//append no main dos quadrados
+}
+
+///////////////////////////////////////////////////////////
+function setCor(elemento){
+    //console.log("in")
+    const cor = getRandomCor()
+    //console.log(cor)
+    elemento.style.background = cor
+    elemento.style.boxShadow = `0 0 2px ${cor}, 0 0 10px ${cor}`
+}
+
+function removerCor(elemento){
+   //console.log("out")
+    elemento.style.background = '#1d1d1d'
+    elemento.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomCor(){
+    return cores[Math.floor(Math.random() * cores.length)]
 }
