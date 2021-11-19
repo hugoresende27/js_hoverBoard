256afa1bec7194e5d1cdf96ee8d45d4480c4d2da
hugoresende27 <Hugoresende27@gmail.com>
2021-11-19 16:40:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const conteudo = document.
+const conteudo = document.getElementById('main')
+const cores = ['#e74c3c', '#8e44ad', '#3498db','#e67e22', '#2ecc71']
+const QUADRADOS = 500
+
+for (let i=0; i < QUADRADOS ; i++)
+{
+    const quadrado = document.createElement('div')
+    quadrado.classList.add('quadrado')
+
+    conteudo.appendChild(quadrado)
+}
